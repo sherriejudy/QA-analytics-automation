a9acb53740313129e13720a90b96a80f8556b9cd
Adesh <adeshkadambi@gmail.com>
2019-06-24 14:58:53 -0400

    Update Analytics.py

--- Analytics.py
+++ Analytics.py
@@ -10,28 +10,20 @@ import platform
 opSys = platform.system()
 
 if opSys == 'Windows':
-    print('Running on Windows...')
-    # Home directory system path
-    homeDir = os.path.expanduser('~')
-    # System path to the cloned repository (eg. /Users/JohnDoe/Documents/shaw-data-bot)
-    repoPath = os.path.dirname(os.path.abspath(__file__))
-    # Website to be tested as a command line argument
-    websiteURL = sys.argv[1]
-
-    # Finding all navigation links and the associated analytics data
-    PageLoads.endPointHits(websiteURL, homeDir, repoPath + '\adobe-debugger', repoPath + '\chromedriver')
-    # Processing and collating analytics data
-    Processing.CSV_prettifier(homeDir + '\Downloads')
+    slash = '\'
 else:
-    print('Running on Darwin...')
-    # Home directory system path
-    homeDir = os.path.expanduser('~')
-    # System path to the cloned repository (eg. /Users/JohnDoe/Documents/shaw-data-bot)
-    repoPath = os.path.dirname(os.path.abspath(__file__))
-    # Website to be tested as a command line argument
-    websiteURL = sys.argv[1]
+    slash = '/'
 
-    # Finding all navigation links and the associated analytics data
-    PageLoads.endPointHits(websiteURL, homeDir, repoPath + '/adobe-debugger', repoPath + '/chromedriver')
-    # Processing and collating analytics data
-    Processing.CSV_prettifier(homeDir + '/Downloads')
+
+print('Running on', opSys,'...')
+# Home directory system path
+homeDir = os.path.expanduser('~')
+# System path to the cloned repository (eg. /Users/JohnDoe/Documents/shaw-data-bot)
+repoPath = os.path.dirname(os.path.abspath(__file__))
+# Website to be tested as a command line argument
+websiteURL = sys.argv[1]
+
+# Finding all navigation links and the associated analytics data
+PageLoads.endPointHits(websiteURL, homeDir, repoPath + slash + 'adobe-debugger', repoPath + slash + 'chromedriver')
+# Processing and collating analytics data
+Processing.CSV_prettifier(homeDir + slash + 'Downloads')
