b3216646f824fbd744db896b679f4c7aa35fefa1
Adesh <adeshkadambi@gmail.com>
2019-06-24 14:36:22 -0400

    windows compatibility v1

--- Analytics.py
+++ Analytics.py
@@ -5,18 +5,31 @@ import PageLoads
 import Processing
 import os
 import sys
+import platform
 
-# Home directory system path
-homeDir = os.path.expanduser('~')
-# System path to the cloned repository (eg. /Users/JohnDoe/Documents/shaw-data-bot)
-repoPath = os.path.dirname(os.path.abspath(__file__))
-# Website to be tested as a command line argument
-websiteURL = sys.argv[1]
+opSys = platform.system()
 
-# Finding all navigation links and the associated analytics data
-PageLoads.endPointHits(websiteURL, homeDir, repoPath + '/adobe-debugger', repoPath + '/chromedriver')
-# Processing and collating analytics data
-Processing.CSV_prettifier(homeDir + '/Downloads')
+if opSys == 'Windows':
+    # Home directory system path
+    homeDir = os.path.expanduser('~')
+    # System path to the cloned repository (eg. /Users/JohnDoe/Documents/shaw-data-bot)
+    repoPath = os.path.dirname(os.path.abspath(__file__))
+    # Website to be tested as a command line argument
+    websiteURL = sys.argv[1]
 
+    # Finding all navigation links and the associated analytics data
+    PageLoads.endPointHits(websiteURL, homeDir, repoPath + '\adobe-debugger', repoPath + '\chromedriver')
+    # Processing and collating analytics data
+    Processing.CSV_prettifier(homeDir + '\Downloads')
+else:
+    # Home directory system path
+    homeDir = os.path.expanduser('~')
+    # System path to the cloned repository (eg. /Users/JohnDoe/Documents/shaw-data-bot)
+    repoPath = os.path.dirname(os.path.abspath(__file__))
+    # Website to be tested as a command line argument
+    websiteURL = sys.argv[1]
 
-
+    # Finding all navigation links and the associated analytics data
+    PageLoads.endPointHits(websiteURL, homeDir, repoPath + '/adobe-debugger', repoPath + '/chromedriver')
+    # Processing and collating analytics data
+    Processing.CSV_prettifier(homeDir + '/Downloads')
