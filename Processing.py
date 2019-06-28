cbbc8666ae4817423239a8acf92de98f9f33b0b2
akadambi <adeshkadambi@gmail.com>
2019-06-28 01:43:54 -0400

    updated csv prettifier for form filling

--- Processing.py
+++ Processing.py
@@ -37,7 +37,7 @@ def prodStr (df):
         count = count + 1
     return prostr
 
-def CSV_prettifier(path, forms = False):
+def CSV_prettifier(path, endpoints, outfile, forms = False):
 
     """
     Input: path of raw .csv files from adobe debugger extension.
@@ -48,16 +48,18 @@ def CSV_prettifier(path, forms = False):
     import pandas as pd
     import os
     import glob
-    from pathlib import Path, PureWindowsPath
+    from pathlib import Path
 
     # Read all csv files in specified path.
-    all_files = glob.glob1(path,"*csv")
+    all_files = glob.glob1(path, "*csv")
 
-    ep = pd.read_csv(path+ '/Endpoints.csv', names=['Endpoints'])
-    dup_ep = ep
-    dup_ep['Result'] = 'FAILURE/REDIRECTED'
+    # If forms is set to False, open endpoints and set all links to 'FAILED' status.
+    if not forms:
+        ep = pd.read_csv(str(Path(path + '/' + endpoints)), names=['Endpoints'])
+        dup_ep = ep
+        dup_ep['Result'] = 'FAILURE/REDIRECTED'
 
-    # Remove any csv files that aren't part of analytics output.
+    # Remove any csv files that aren't part of analytics output from the list (doesn't delete files).
     for file in all_files:
         if file[0:24] == 'adobe-analytics-data-raw':
             pass
@@ -67,50 +69,52 @@ def CSV_prettifier(path, forms = False):
     li = []
     di = {}
 
-    # Read csv files to pandas and append to `li` for summary page and `di` for indiv sheets.
+    # Read csv files to pandas and append to `li` for summary page and `di` for individual sheets.
     for filename in all_files:
-        x = path+ '/' +filename
+        x = str(Path(path + '/' + filename))
         df = pd.read_csv(x, index_col=None, header=0, sep='~~', engine='python')
-        df['Report Suite ID    '] = df['Report Suite ID    '].apply(lambda x:x.strip())
+        df['Report Suite ID    '] = df['Report Suite ID    '].apply(lambda x: x.strip())
         df.set_index('Report Suite ID    ', inplace=True)
 
+        # Checks if 'Products' index exists and prints to a text file (keeps appending).
         if 'Products' in df.index:
             product_str = prodStr(df)
             df.loc['Products'][0] = product_str
-            outF = open(path+"product-strings.txt", "a")
-            print >>outF, product_str
-            outF.close()
+            with open(str(Path(path + '/' + 'product-strings.txt')), "a") as text_file:
+                print(product_str, file=text_file)
+            text_file.close()
 
         li.append(df)
 
-        # If URL is isn't in the endpoints file, FAILED.
-        url = df.loc['Current URL        '][0].strip()
-        if url[-1] == '/':
-            url = url[:-1]
+        # If we aren't form filling, then compare current URL to endpoints URLs.
+        if not forms:
+            # If URL is isn't in the endpoints file, FAILED.
+            url = df.loc['Current URL        '][0].strip()
+            if url[-1] == '/':
+                url = url[:-1]
 
-        if url in list(ep['Endpoints']):
-            i = ep[ep['Endpoints'] == url].index
-            dup_ep['Result'][i] = 'SUCCESS'
-
+            if url in list(ep['Endpoints']):
+                i = ep[ep['Endpoints'] == url].index
+                dup_ep['Result'][i] = 'SUCCESS'
 
         # Setting the name of each sheet to the webpage name.
         if len(df.iloc[0][0]) <= 31:
-            di.update({ df.iloc[0][0] : df})
+            di.update({df.iloc[0][0]: df})
         elif len(df.iloc[0][0]) > 31:
-            di.update({ 'home' : df})
+            di.update({'home': df})
 
         # Delete raw csv file after use.
         os.remove(x)
 
     # Concat list of dataframes to generate summary page.
     frame = pd.concat(li, axis=1, sort='False')
-    di.update({'Summary' : frame})
+    di.update({'Summary': frame})
 
-    writer = pd.ExcelWriter(path+ '/' +'Shaw-pageloads.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter(str(Path(path + '/' + outfile)), engine='xlsxwriter')
 
     # Write each sheet to .xlsx file.
     for sheet in di.keys():
         di[sheet].to_excel(writer, sheet_name=sheet, index=True)
 
-    dup_ep.to_csv(path+ '/' +'Endpoints-final.csv',index=False)
+    dup_ep.to_csv(str(Path(path + '/' + endpoints)), index=False)
     writer.save()
